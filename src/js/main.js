608391d3659fd5604a5fbedf4a3615536d2e430e
Juan <cabraljcruz@gmail.com>
2019-10-02 04:52:25 -0300

    Added Javascript

---
+++ src/js/main.js
@@ -0,0 +1,41 @@
+var menuBtn = document.getElementById('menu');
+var menuOpen = document.getElementById('menu-slide');
+var closeBtn = document.getElementById('menu-close');
+var laMetroLogo = document.getElementById('la-metro');
+
+menuBtn.onclick = function showMenu(){
+    //menuOpen.style.display = 'grid';
+    var posInit = 100;
+    var animOpen = setInterval(openMove, 1);
+
+    function openMove(){
+        if(posInit <= 0){
+            clearInterval(animOpen);
+        }
+        else{
+            posInit-= 5;
+            menuOpen.style.transform = `translateX(${posInit}%)`;
+            laMetroLogo.style.filter = `brightness(${posInit}%)`;
+        }
+    }
+
+    laMetroLogo.style.position = 'fixed';
+    
+}
+
+closeBtn.onclick = function hideMenu(){
+    var posEnd = 0;
+    var animClose = setInterval(closeMove, 1);
+
+    function closeMove(){
+        if(posEnd >= 100){
+            clearInterval(animClose)
+        }
+        else{
+            posEnd += 5;
+            menuOpen.style.transform = `translateX(${posEnd}%)`;
+            laMetroLogo.style.filter = `brightness(${posEnd}%)`;
+        }
+    }
+}
+
